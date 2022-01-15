7cf644daedd55537aaf1e23fa6d020dc3a636393
Marcelo <marsleite@gmail.com>
2022-01-15 11:49:40 -0300

    instalação de dependencias do graphql

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
-const app = express();
+const main = async () => {
+  const app = express();
 
-const PORT = 4000;
+  const PORT = 4000;
 
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+  });
+};
+
+main().catch((error) => console.log(error));
