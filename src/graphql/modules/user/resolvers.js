57f87a28bec627fd309794baa00752cba671eae9
Marcelo <marsleite@gmail.com>
2022-01-17 15:31:17 -0300

    implementar o update do usuario

--- src/graphql/modules/user/resolvers.js
+++ src/graphql/modules/user/resolvers.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const md5 = require('md5');
-const { UserMiddleware } = require('../../../middleware/user/user.middleware');
+// const { UserMiddleware } = require('../../../middleware/user/user.middleware');
 
 module.exports = {
   User: {
@@ -28,7 +28,7 @@ module.exports = {
       const userData = {
         firstName, lastName, email, password: md5(password), cpf, birthDay,
       };
-      await new UserMiddleware(email).alreadyExists();
+      // await new UserMiddleware(email).alreadyExists();
 
       const newUser = await new PrismaClient().user.create({
         data: {
@@ -41,6 +41,31 @@ module.exports = {
         },
       });
       return newUser;
+    },
+    updateUser: async (_, args) => {
+      const {
+        id, firstName, lastName, email, password, cpf, birthDay, ...addresses
+      } = args;
+      const userData = {
+        firstName, lastName, email, password: md5(password), cpf, birthDay,
+      };
+      const updatedUser = await new PrismaClient().user.update({
+        where: {
+          id,
+        },
+        data: {
+          ...userData,
+        },
+      });
+      const updatedAddress = await new PrismaClient().address.update({
+        where: {
+          clientId: id,
+        },
+        data: {
+          ...addresses,
+        },
+      });
+      return { ...updatedUser, address: updatedAddress };
     },
   },
 };
